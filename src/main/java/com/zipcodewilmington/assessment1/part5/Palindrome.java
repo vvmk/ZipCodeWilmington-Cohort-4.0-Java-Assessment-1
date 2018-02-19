d3ac5ff135132a17b7afecfd55b3d390d024f25f
vvmk <vincentmasiello@gmail.com>
2018-02-19 16:36:56 -0500

    part5 green except the last test (wow)

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,28 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import com.zipcodewilmington.assessment1.part1.BasicStringUtils;
+
+import java.util.ArrayList;
+
 public class Palindrome {
 
-    public Integer countPalindromes(String input){
-        return null;
+    public Integer countPalindromes(String input) {
+        int total = 0;
+        for (String s : getAllSubstrings(input)) {
+            if (s.equals(BasicStringUtils.reverse(s)))
+                total++;
+        }
+        return total;
+    }
+
+    private String[] getAllSubstrings(String input) {
+        ArrayList<String> sol = new ArrayList<>();
+        for (int startIndex = 0;startIndex<input.length();startIndex++) {
+            for (int endIndex = startIndex;endIndex<input.length();endIndex++) {
+                sol.add(input.substring(startIndex, endIndex+1)); //String.substring() end index is non inclusive, +1
+            }
+        }
+        System.out.println(sol.toString());
+        return sol.toArray(new String[0]);
     }
 }
